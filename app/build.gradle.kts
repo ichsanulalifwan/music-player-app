4b8eedaf6ca8440b473313fd24ad4f0cb7f2e966
ichsanulalifwan <Ichsanul.Alifwan@maybank.co.id>
2025-05-05 00:33:51 +0700

    add firebase config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,12 +41,16 @@ android {
 }
 
 dependencies {
+//    implementation(project.core.ui)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
 
     // DI
     implementation(libs.dagger.hilt)
